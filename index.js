a35eb27bee1a91c8c813c0d0538b42fc7d889dae
Egor <zangedzu333gig@gmail.com>
2018-09-23 20:06:59 +0300

    Add handlers and auto save articles

--- index.js
+++ index.js
@@ -3,28 +3,34 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const articles = require('./articles.json');
+
 const handlers = {
-  '/sum': sum
+	'/api/articles/readall': readAll,
+	'/api/articles/read' : read,
+	'/api/articles/create' : createArticle,
+	'/api/articles/update' : updateArticle,
+	'/api/articles/delete' : deleteArticle,
+	'/api/comments/create' : createComment,
+	'/api/comments/delete' : deleteComment
 };
 
 const server = http.createServer((req, res) => {
-  parseBodyJson(req, (err, payload) => {
-    const handler = getHandler(req.url);
-
-    handler(req, res, payload, (err, result) => {
-      if (err) {
-        res.statusCode = err.code;
-        res.setHeader('Content-Type', 'application/json');
-        res.end( JSON.stringify(err) );
-
-        return;
-      }
-
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.end( JSON.stringify(result) );
-    });
-  });
+	parseBodyJson(req, (err, payload) => {
+		const handler = getHandler(req.url);
+		handler(req, res, payload, (err, result) => {
+			if (err) {
+				res.statusCode = err.code;
+				res.setHeader('Content-Type', 'application/json');
+				res.end(JSON.stringify(err));
+				return;
+			}
+			fs.createWriteStream('articles.json').write(JSON.stringify(articles));
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			res.end(JSON.stringify(result));
+		});
+	});
 });
 
 server.listen(port, hostname, () => {
@@ -32,29 +38,20 @@ server.listen(port, hostname, () => {
 });
 
 function getHandler(url) {
-  return handlers[url] || notFound;
-}
-
-function sum(req, res, payload, cb) {
-  const result = { c: payload.a + payload.b };
-
-  cb(null, result);
+	return handlers[url] || notFound;
 }
 
 function notFound(req, res, payload, cb) {
-  cb({ code: 404, message: 'Not found'});
+	cb({ code: 404, message: 'Not found'});
 }
 
 function parseBodyJson(req, cb) {
-  let body = [];
-
-  req.on('data', function(chunk) {
-    body.push(chunk);
-  }).on('end', function() {
-    body = Buffer.concat(body).toString();
-
-    let params = JSON.parse(body);
-
-    cb(null, params);
-  });
+	let body = [];
+	req.on('data', (chunk) => {
+		body.push(chunk);
+	}).on('end', () => {
+		body = Buffer.concat(body).toString();
+		let params = JSON.parse(body);
+		cb(null, params);
+	});
 }
